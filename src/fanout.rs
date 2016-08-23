61d276bcb42e7fc176ae7c1fc74049665324b9ea
Yong Wen Chua <ida@yongwen.xyz>
2016-08-23 09:48:30 +0800

    Use mutex instead of RwLock

--- src/fanout.rs
+++ src/fanout.rs
@@ -1,50 +1,47 @@
 use std::sync::mpsc::{channel, Sender, Receiver};
-use std::sync::Arc;
-use std::sync::RwLock;
+use std::sync::{Arc, Mutex};
 use std::thread::{spawn, JoinHandle};
 use std::marker::Send;
 
+#[derive(RustcDecodable, RustcEncodable, Debug, Clone)]
+pub struct Message {
+    message_type: String,
+    text: String
+}
+
 pub struct Fanout<T> where T : 'static + Send + Sync + Clone {
-    sender: Sender<T>,
-    subscribers: Arc<RwLock<Vec<Sender<T>>>>
+    broadcast_tx: Sender<T>,
+    subscribers: Arc<Mutex<Vec<Sender<T>>>>
 }
 
 impl<T> Fanout<T>  where T : 'static + Send + Sync + Clone {
     fn new() -> Fanout<T> {
-        let (sender, receiver) = channel::<T>();
-        let subscribers: Vec<Sender<T>> = vec![];
-        let lock = RwLock::new(subscribers);
-        let arc = Arc::new(lock);
+        let (broadcast_tx, broadcast_rx) = channel::<T>();
+        let subscribers = Arc::new(Mutex::new(Vec::<Sender<T>>::new()));
 
-        let clone_arc = arc.clone();
+        let cloned_subscribers = subscribers.clone();
         spawn(move || {
-            loop {
-                match receiver.recv() {
-                    Ok(payload) => {
-                        let lock = *clone_arc.read();
-                        match lock {
-                            Ok(subscribers) => {},
-                            Err(_) => break
-                        }
-                    },
-                    Err(_) => break
-                };
+            println!("Broadcast loop");
+            for message in broadcast_rx.iter() {
+                for subscriber_tx in cloned_subscribers.lock().unwrap().iter() {
+                    subscriber_tx.send(message.clone());
+                }
             }
         });
 
         Fanout {
-            sender: sender,
-            subscribers: arc
+            broadcast_tx: broadcast_tx,
+            subscribers: subscribers
         }
     }
 
-    // fn subscribe(&mut self) -> Receiver<T> {
-    //     let (tx, rx) = channel::<T>();
-    //     self.subscribers.push(tx);
-    //     rx
-    // }
+    fn subscribe(&mut self) -> Receiver<T> {
+        let (tx, rx) = channel::<T>();
+        self.subscribers.lock().unwrap().push(tx);
+        rx
+    }
 
-    fn broadcast() {
-
+    fn broadcast(&self, message: &T) {
+        self.broadcast_tx.send(message.clone());
     }
 }
